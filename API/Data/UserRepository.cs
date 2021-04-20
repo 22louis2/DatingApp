dc8d26871752c226ca7daa212d57321b5a226a2c
22louis2 <otulouisakhigbe@gmail.com>
2021-04-20 23:38:06 +0100

    End of section 14

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API.Services
+namespace API.Data
 {
   public class UserRepository : IUserRepository
   {
