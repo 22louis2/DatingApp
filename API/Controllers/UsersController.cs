d511b09e3892b9868d9c3e4a5b486767f165ece0
22louis2 <otulouisakhigbe@gmail.com>
2021-04-29 11:07:01 +0100

    Penultimate

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -51,7 +51,8 @@ namespace API.Controllers
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _unitOfWork.UserRepository.GetMemberAsync(username);
+            var currentUsername = User.GetUsername();
+            return await _unitOfWork.UserRepository.GetMemberAsync(username, isCurrentUser: currentUsername == username);
         }
 
         [HttpPut]
@@ -82,15 +83,12 @@ namespace API.Controllers
                 PublicId = result.PublicId
             };
 
-            if (user.Photos.Count == 0)
-            {
-                photo.IsMain = true;
-            }
-
             user.Photos.Add(photo);
 
             if (await _unitOfWork.Complete())
-            return CreatedAtRoute("GetUser", new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
+            return CreatedAtRoute("GetUser", 
+                new { username = user.UserName }, 
+                _mapper.Map<PhotoDto>(photo));
 
             return BadRequest("Problem adding photo");
         }
